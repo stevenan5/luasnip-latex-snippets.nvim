e6490e5c06f8ee8fb16f99cf7b758c37ae310ad7
Steven An <sla001@eng.ucsd.edu>
2025-05-19 02:16:35 -0700

    fix collisions

--- lua/luasnip-latex-snippets/bwA.lua
+++ lua/luasnip-latex-snippets/bwA.lua
@@ -26,9 +26,9 @@ function M.retrieve(not_math)
     ),
 
     parse_snippet({ trig = "beg", name = "begin{} / end{}" }, "\\begin{$1}\n\t$0\n\\end{$1}"),
-    parse_snippet({ trig = "itemi", name = "Itemize" }, "\\begin{itemize}\n\t\\item $0\n\\end{itemize}"),
-    parse_snippet({ trig = "enum", name = "Enumerate" }, "\\begin{enumerate}\n\t\\item $0\n\\end{enumerate}"),
-    parse_snippet({ trig = "item", name = "Item" }, "\\item "),
+    parse_snippet({ trig = "ite", name = "Itemize" }, "\\begin{itemize}\n\t\\item $0\n\\end{itemize}"),
+    parse_snippet({ trig = "enu", name = "Enumerate" }, "\\begin{enumerate}\n\t\\item $0\n\\end{enumerate}"),
+    parse_snippet({ trig = "itm", name = "Item" }, "\\item "),
 
     s({ trig = "bigfun", name = "Big function" }, {
       t({ "\\begin{align*}", "\t" }),
