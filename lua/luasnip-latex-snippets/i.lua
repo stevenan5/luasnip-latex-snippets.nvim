d29d6abcbc60c55c9d3507fe48f357dc436d2440
Steven An <sla001@eng.ucsd.edu>
2025-05-19 02:10:06 -0700

    remove spaces

--- lua/luasnip-latex-snippets/i.lua
+++ lua/luasnip-latex-snippets/i.lua
@@ -12,13 +12,13 @@ function M.retrieve(not_math)
   }) --[[@as function]]
 
   return {
-    parse_snippet({ trig = "lab", name = "label" }, "~\\label_{${1:${TM_SELECTED_TEXT}}} $0"),
-    parse_snippet({ trig = "ref", name = "ref" }, "~\\ref_{${1:${TM_SELECTED_TEXT}}} $0"),
-    parse_snippet({ trig = "cite", name = "cite" }, "~\\cite_{${1:${TM_SELECTED_TEXT}}} $0"),
-    parse_snippet({ trig = "citep", name = "citep" }, "~\\citep_{${1:${TM_SELECTED_TEXT}}} $0"),
-    parse_snippet({ trig = "citet", name = "citet" }, "~\\citet_{${1:${TM_SELECTED_TEXT}}} $0"),
-    parse_snippet({ trig = "bol", name = "bold" }, "~\\textbf_{${1:${TM_SELECTED_TEXT}}} $0"),
-    parse_snippet({ trig = "ita", name = "italic" }, "~\\textit_{${1:${TM_SELECTED_TEXT}}} $0"),
+    parse_snippet({ trig = "lab", name = "label" }, "~\\label{${1:${TM_SELECTED_TEXT}}} $0"),
+    parse_snippet({ trig = "ref", name = "ref" }, "~\\ref{${1:${TM_SELECTED_TEXT}}} $0"),
+    parse_snippet({ trig = "cite", name = "cite" }, "~\\cite{${1:${TM_SELECTED_TEXT}}} $0"),
+    parse_snippet({ trig = "citep", name = "citep" }, "~\\citep{${1:${TM_SELECTED_TEXT}}} $0"),
+    parse_snippet({ trig = "citet", name = "citet" }, "~\\citet{${1:${TM_SELECTED_TEXT}}} $0"),
+    parse_snippet({ trig = "bol", name = "bold" }, "\\textbf{${1:${TM_SELECTED_TEXT}}} $0"),
+    parse_snippet({ trig = "ita", name = "italic" }, "\\textit{${1:${TM_SELECTED_TEXT}}} $0"),
   }
 end
 
