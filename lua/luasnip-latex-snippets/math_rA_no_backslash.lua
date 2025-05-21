85760356eb22807c127e7a1e6aef5ae5fd1952ca
Steven An <sla001@eng.ucsd.edu>
2025-05-20 18:26:12 -0700

    add xi

--- lua/luasnip-latex-snippets/math_rA_no_backslash.lua
+++ lua/luasnip-latex-snippets/math_rA_no_backslash.lua
@@ -37,7 +37,7 @@ end
 
 local greek_postfix_completions = function()
   local re =
-    "[aA]lpha|[bB]eta|[cC]hi|[dD]elta|[eE]psilon|[gG]amma|[iI]ota|[kK]appa|[lL]ambda|[mM]u|[nN]u|[oO]mega|[pP]hi|[pP]i|[pP]si|[rR]ho|[sS]igma|[tT]au|[tT]heta|[zZ]eta|[eE]ta"
+    "[aA]lpha|[bB]eta|[cC]hi|[dD]elta|[eE]psilon|[gG]amma|[iI]ota|[kK]appa|[lL]ambda|[mM]u|[nN]u|[oO]mega|[pP]hi|[pP]i|[pP]si|[rR]ho|[sS]igma|[tT]au|[tT]heta|[zZ]eta|[eE]ta|[Xx]i"
 
   local build = build_with_priority(postfix_trig, postfix_node, 200)
   return vim.tbl_map(build, vim.split(re, "|"))
