392b54953a362b7243c75124392116d3ac9a0d50
Steven An <sla001@eng.ucsd.edu>
2025-05-20 18:25:13 -0700

    reorder quad snippets

--- lua/luasnip-latex-snippets/math_iA.lua
+++ lua/luasnip-latex-snippets/math_iA.lua
@@ -213,8 +213,8 @@ function M.retrieve(is_math)
 
     parse_snippet({ trig = "stt", name = "text subscript" }, "_\\text{$1} $0"),
     parse_snippet({ trig = "tt", name = "text" }, "\\text{$1}$0"),
-    parse_snippet({ trig = "qu", name = "quad" }, "\\quad $0"),
     parse_snippet({ trig = "qqu", name = "qquad" }, "\\qquad $0"),
+    parse_snippet({ trig = "qu", name = "quad" }, "\\quad $0"),
 
     parse_snippet({ trig = "xx", name = "cross" }, "\\times "),
 
