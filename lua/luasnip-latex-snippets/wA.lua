e55f2d83646e9c649b927ed4138afdbfd9e122da
Steven An <sla001@eng.ucsd.edu>
2025-05-20 17:24:31 -0700

    add ie, eg snippets

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -11,12 +11,19 @@ function M.retrieve(not_math)
 
   return {
     parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\) $0"),
-    parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\]\n$0"),
+    parse_snippet(
+      { trig = "dm", name = "Block Math" },
+      "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\]\n$0"
+    ),
     parse_snippet({ trig = " .", name = "Period" }, "."),
     parse_snippet({ trig = " ,", name = "Comma" }, ","),
     parse_snippet({ trig = " '", name = "Apostrophe" }, "'"),
     parse_snippet({ trig = " !", name = "Exclam. Mark" }, "!"),
     parse_snippet({ trig = " ?", name = "Question Mark" }, "?"),
+    parse_snippet({ trig = "ie", name = "ie" }, "i.e.~"),
+    parse_snippet({ trig = "Ie", name = "Ie" }, "I.e.~"),
+    parse_snippet({ trig = "eg", name = "eg" }, "e.g.~"),
+    parse_snippet({ trig = "Eg", name = "Eg" }, "E.g.~"),
   }
 end
 
