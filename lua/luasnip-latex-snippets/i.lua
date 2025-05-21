95c7e99c15909722a4e892f833af3760d4ac6ae9
Steven An <sla001@eng.ucsd.edu>
2025-05-20 17:20:06 -0700

    hopefully fix bug with pac snippet

--- lua/luasnip-latex-snippets/i.lua
+++ lua/luasnip-latex-snippets/i.lua
@@ -12,10 +12,7 @@ function M.retrieve(not_math)
   }) --[[@as function]]
 
   return {
-    ls.parser.parse_snippet(
-      { trig = "pac", name = "Package" },
-      "\\usepackage[${1:options}]{${2:package}}$0"
-    ),
+    parse_snippet({ trig = "pac", name = "Package" }, "\\usepackage[${1:options}]{${2:package}}$0"),
     parse_snippet({ trig = "lab", name = "label" }, "~\\label{${1:${TM_SELECTED_TEXT}}} $0"),
     parse_snippet({ trig = "ref", name = "ref" }, "~\\ref{${1:${TM_SELECTED_TEXT}}} $0"),
     parse_snippet({ trig = "cite", name = "cite" }, "~\\cite{${1:${TM_SELECTED_TEXT}}} $0"),
