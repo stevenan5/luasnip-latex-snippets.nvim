46f487f6b608b1f26ddc3e34842bcc679c1fdce3
Steven An <sla001@eng.ucsd.edu>
2025-10-01 21:14:24 -0700

    deactivate snippets for auto subscript

--- lua/luasnip-latex-snippets/math_wrA.lua
+++ lua/luasnip-latex-snippets/math_wrA.lua
@@ -38,7 +38,11 @@ local frac_node = {
     if depth ~= 0 then
       return string.format("%s\\frac{}", stripped)
     else
-      return string.format("%s\\frac{%s}", stripped:sub(1, i - 1), stripped:sub(i + 1, #stripped - 1))
+      return string.format(
+        "%s\\frac{%s}",
+        stripped:sub(1, i - 1),
+        stripped:sub(i + 1, #stripped - 1)
+      )
     end
   end, {}),
   t("{"),
@@ -73,16 +77,16 @@ function M.retrieve(is_math)
   }) --[[@as function]]
 
   local snippets = {
-    s({
-      trig = "([%a])(%d)",
-      name = "auto subscript",
-    }, vim.deepcopy(subscript_node)),
-
-    s({
-      trig = "([%a])_(%d%d)",
-      name = "auto subscript 2",
-    }, vim.deepcopy(subscript_node)),
-
+    -- s({
+    --   trig = "([%a])(%d)",
+    --   name = "auto subscript",
+    -- }, vim.deepcopy(subscript_node)),
+    --
+    -- s({
+    --   trig = "([%a])_(%d%d)",
+    --   name = "auto subscript 2",
+    -- }, vim.deepcopy(subscript_node)),
+    --
     s({
       priority = 1000,
       trig = ".*%)/",
